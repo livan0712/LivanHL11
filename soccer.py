dc7a3043c4bb551b23f0ffc8e2d0255ecc63dcbc
livan0712 <61759693+livan0712@users.noreply.github.com>
2020-03-03 15:37:35 -0600

    Create soccer.py
    soccer file

---
+++ soccer.py
@@ -0,0 +1,17 @@
+DIVIDER="*****************************"
+AGE="19"
+NAME="LIVAN"
+LAST_NAME="AGUILAR"
+ID ='193111844'
+SPORT="SOCCER"
+HEIGHT="1.77"
+SCHOLARSHIP= True
+print(DIVIDER)
+print('{:10}{}'.format('ID:', ID))
+print(DIVIDER)
+print('{:10}{} {}'.format('NAME: ', LAST_NAME, NAME))
+print('{:10}{}'.format('AGE: ', AGE))
+print('{:10}{}'.format('HEIGHT: ', HEIGHT))
+print('{:10}{}'.format('SPORT: ', SPORT))
+print('{:10}{}'.format('SCHOLARSHIP: ', SCHOLARSHIP))
+print(DIVIDER)
